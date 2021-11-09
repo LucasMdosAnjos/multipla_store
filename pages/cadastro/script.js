33fba61c3ad3e39b397bbc4ee83491c9f094e8a0
Natan Santos Bosso <bosso.natan@chavi.com.br>
2021-11-09 19:56:48 -0300

    Incluido JS em cadastro e login

---
+++ pages/cadastro/script.js
@@ -0,0 +1,17 @@
+function gravarFormularioCadastro() {
+    // Variaveis que pegam o id de cada input
+    var nome = document.getElementById("name");
+    var email = document.getElementById("email");
+    var senha = document.getElementById("password");
+
+    if (nome.value == "" || email.value == "" || senha.value == "") {
+        alert("Todos os campos devem ser preenchidos!");
+        print('Todos os campos devem ser preenchidos!');
+
+        return;
+    } else {
+        alert("Os dados foram gravados com sucesso");
+        print('Os dados foram gravados com sucesso');
+        // envia o formulário
+    }
+}
